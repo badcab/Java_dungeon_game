2e2aa67cf5fd12f232ae36541ede75b49add18f8
Michael Spear <github@bsd4breakfast.us>
2013-12-16 13:46:55 -0600

    Update Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -1,50 +1,49 @@
-
 public interface Equipment {
 	/**
 	 * 
-	 * @return
+	 * @return the attack bonus for equipment
 	 */
 	int getAttackBonus();
 	
 	/**
 	 * 
-	 * @return
+	 * @return the armmor bonus for equipment
 	 */
 	int getArmorBonus();
 	
 	/**
 	 * 
-	 * @return
+	 * @return return speed bonus for equipment
 	 */
 	int getSpeedBonus();
 	
 	/**
 	 * 
-	 * @return
+	 * @return human readable text about equipment stats
 	 */
 	String printStats();
 	
 	/**
 	 * 
-	 * @return
+	 * @return return required strenght needed by player interface to use equipment
 	 */
 	int getRequiredStrength();
 	
 	/**
 	 * 
-	 * @return
+	 * @return return required dextarity needed by player interface to use equipment
 	 */
 	int getRequiredDex();
 	
 	/**
 	 * 
-	 * @return
+	 * @return name of equipment
 	 */
 	String getName();
 	
 	/**
 	 * 
-	 * @return
+	 * @return return non human readable stats on equipment
 	 */
 	String getStats();
 }
